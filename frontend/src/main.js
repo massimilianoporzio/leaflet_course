94984304be7dfc580130edfcd2620258e84baff7
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-11-30 17:24:41 +0100

    da casa con copia in public per vite

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,24 +1,57 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Fix per le icone di Leaflet con bundler - punta alla cartella public
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: '/marker-icon-2x.png',
+  iconUrl: '/marker-icon.png',
+  shadowUrl: '/marker-shadow.png',
+});
 
-setupCounter(document.querySelector('#counter'))
+window.onload = init;
+
+function init() {
+  const mapElement = document.getElementById('mapid');
+
+  const stadiaMaps = L.tileLayer(
+    'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}',
+    {
+      attribution:
+        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      ext: 'png',
+    }
+  );
+
+  const openstreetmapStandard = L.tileLayer(
+    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
+  );
+
+  const myMap = L.map(mapElement, {
+    center: [48, 14],
+    zoom: 5,
+    minZoom: 4,
+    layers: [openstreetmapStandard],
+  });
+
+  const baseLayers = {
+    'OpenStreetMap Standard': openstreetmapStandard,
+    'Stadia Maps - Alidade Smooth Dark': stadiaMaps,
+  };
+
+  const layerControls = L.control
+    .layers(
+      baseLayers,
+      {},
+      {
+        collapsed: true,
+      }
+    )
+    .addTo(myMap);
+  console.log('Map initialized');
+}
